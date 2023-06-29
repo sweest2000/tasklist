d27d701abd28cd5db3622ebbeb56b4bc49319eb1
Edgar Sadykhov <madisnormal@gmail.com>
2023-06-29 09:16:05 +0300

    Minor fixes for Netlify

--- src/data/handler.js
+++ src/data/handler.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
+const { REACT_APP_SUPABASE_KEY } = process.env;
+
 export const db = createClient(
   'https://iqtsptfitoeapbmotenf.supabase.co',
-  process.env.REACT_APP_SUPABASE_KEY
+  REACT_APP_SUPABASE_KEY
 );
 
 export default db;
